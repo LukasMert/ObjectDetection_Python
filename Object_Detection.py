57a5963213c1c505223dae312d1826a76f7e8861
Lukas Mertens <Lukas.mertens@outlook.com>
2021-03-26 09:43:47 +0100

    Updated program

--- Object_Detection.py
+++ Object_Detection.py
@@ -1,4 +1,5 @@
 import cv2
+import RPi.GPIO as GPIO
 
 ################################################################
 path = '../haarcascade/cascade.xml'  # PATH OF THE CASCADE
@@ -9,6 +10,8 @@ frameHeight = 480  # DISPLAY HEIGHT
 color = (255, 0, 255)
 #################################################################
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
 
 cap = cv2.VideoCapture(cameraNo)
 cap.set(3, frameWidth)
@@ -22,23 +25,30 @@ def empty(a):
 # CREATE TRACKBAR
 cv2.namedWindow("Result")
 cv2.resizeWindow("Result", frameWidth, frameHeight + 100)
-cv2.createTrackbar("Scale", "Result", 400, 1000, empty)
-cv2.createTrackbar("Neig", "Result", 8, 50, empty)
-cv2.createTrackbar("Min Area", "Result", 0, 100000, empty)
-cv2.createTrackbar("Brightness", "Result", 180, 255, empty)
+cv2.createTrackbar("Scale", "Result", 800, 1000, empty)
+cv2.createTrackbar("Neig", "Result", 33, 50, empty)
+cv2.createTrackbar("Min Area", "Result", 1000, 100000, empty)
+cv2.createTrackbar("Brightness", "Result", 60, 255, empty)
 
 # LOAD THE CLASSIFIERS DOWNLOADED
 cascade = cv2.CascadeClassifier(path)
 
+#Tracker FPS
+tracker= cv2.TrackerCSRT_create()
+
 while True:
+    #FPS Camera
+    timer = cv2.getTickCount()
+    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
     # SET CAMERA BRIGHTNESS FROM TRACKBAR VALUE
     cameraBrightness = cv2.getTrackbarPos("Brightness", "Result")
     cap.set(10, cameraBrightness)
     # GET CAMERA IMAGE AND CONVERT TO GRAYSCALE
     success, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#    cv2.putText(img, str(int(fps)), (75, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2);
     # DETECT THE OBJECT USING THE CASCADE
-    scaleVal = 1 + (cv2.getTrackbarPos("Scale", "Result") / 1000)
+    scaleVal = 2 + (cv2.getTrackbarPos("Scale", "Result") / 1000)
     neig = cv2.getTrackbarPos("Neig", "Result")
     objects = cascade.detectMultiScale(gray, scaleVal, neig)
     # DISPLAY THE DETECTED OBJECTS
@@ -49,10 +59,11 @@ while True:
             cv2.rectangle(img, (x, y), (x + w, y + h), color, 3)
             cv2.putText(img, objectName, (x, y - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, color, 2)
             roi_color = img[y:y + h, x:x + w]
+            GPIO.output(17, GPIO.HIGH)
+        else:
+            GPIO.output(17, GPIO.LOW)
 
     cv2.imshow("Result", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-
 
